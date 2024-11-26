e732eeeb361a91b1a1789888477569639af55606
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2024-11-26 22:00:21 +0500

    README Update

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ db_path = os.path.join(base_dir, 'database/parsers.db')
 
 def get_file_names_enum():
     files = [file for file in os.listdir(parsers_path) if os.path.isfile(os.path.join(parsers_path, file))]
-    return enum.Enum('Parsers', {file.replace('.py', ''): file for file in files})
+    return enum.Enum('Парсеры', {file.replace('.py', ''): file for file in files})
 
 
 # Генерация enum для выпадающего списка
